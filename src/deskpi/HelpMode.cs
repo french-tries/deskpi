a28e88785cdc0e9b98f4fb1a982b0b576dfe93d5
french-tries <56042807+french-tries@users.noreply.github.com>
2021-01-12 10:21:04 -0500

    fix help mode ordering

--- src/deskpi/HelpMode.cs
+++ src/deskpi/HelpMode.cs
@@ -10,15 +10,29 @@ namespace deskpi
 
         public HelpMode(ImmutableDictionary<ModeId, ModeData> modes)
         {
-            this.modes = ImmutableArray<ModeData>.Empty.AddRange(modes.Values);
+            this.modes = modes;
+
+            var keys = from id in modes.Keys
+                       orderby (int)id
+                       select id;
+
+            ids = ImmutableArray<ModeId>.Empty.AddRange(keys);
+            currentMode = ids.IndexOf(ModeId.Help);
+            if (currentMode == -1)
+            {
+                currentMode = 0;
+            }
         }
 
-        private HelpMode(ImmutableArray<ModeData> modes,
-            int currentMode, Field currentField)
+        private HelpMode(HelpMode source, 
+            ImmutableDictionary<ModeId, ModeData> modes = null,
+            ImmutableArray<ModeId>? ids = null, int? currentMode = null, 
+            Field? currentField = null)
         {
-            this.modes = modes;
-            this.currentMode = currentMode;
-            this.currentField = currentField;
+            this.modes = modes ?? source.modes;
+            this.ids = ids ?? source.ids;
+            this.currentMode = currentMode ?? source.currentMode;
+            this.currentField = currentField ?? source.currentField;
         }
 
         public ImmutableList<(string, uint)> Text {
@@ -28,19 +42,19 @@ namespace deskpi
                 switch (currentField)
                 {
                     case Field.Name:
-                        text = modes[currentMode].Name;
+                        text = modes[ids[currentMode]].Name;
                         break;
                     case Field.Description:
-                        text = modes[currentMode].Description;
+                        text = modes[ids[currentMode]].Description;
                         break;
                     case Field.HelpText:
-                        text = modes[currentMode].HelpText;
+                        text = modes[ids[currentMode]].HelpText;
                         break;
                     case Field.SongName:
-                        text = modes[currentMode].Song.Name;
+                        text = modes[ids[currentMode]].Song.Name;
                         break;
                     case Field.SongNotes:
-                        text = Song.NotesToString(modes[currentMode].Song.Notes);
+                        text = Song.NotesToString(modes[ids[currentMode]].Song.Notes);
                         break;
                 }
                 return DeskPiUtils.StringToText(text);
@@ -53,18 +67,18 @@ namespace deskpi
             switch (key)
             {
                 case KeyId.A:
-                    result = new HelpMode(modes, 
-                        currentMode <= 0 ? modes.Length - 1 : currentMode - 1,
-                        Field.Name);
+                    result = new HelpMode(this,
+                        currentMode: currentMode <= 0 ? ids.Length - 1 : currentMode - 1,
+                        currentField: Field.Name);
                     break;
                 case KeyId.B:
-                    result = new HelpMode(modes, currentMode,
+                    result = new HelpMode(this, currentField:
                         currentField + 1 >= Field.Max ? Field.Name : currentField + 1);
                     break;
                 case KeyId.C:
-                    result = new HelpMode(modes,
-                        currentMode + 1 >= modes.Length ? 0 : currentMode + 1,
-                        Field.Name);
+                    result = new HelpMode(this,
+                        currentMode: currentMode + 1 >= ids.Length ? 0 : currentMode + 1,
+                        currentField: Field.Name);
                     break;
             }
             return result;
@@ -74,7 +88,8 @@ namespace deskpi
 
         public IDeskPiMode Tick(uint currentTime) => this;
 
-        private readonly ImmutableArray<ModeData> modes;
+        private readonly ImmutableDictionary<ModeId, ModeData> modes;
+        private readonly ImmutableArray<ModeId> ids;
         private readonly int currentMode = 0;
         private readonly Field currentField = Field.Name;
     }
